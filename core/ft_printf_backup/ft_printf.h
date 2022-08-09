6889896aca25e1925a49aef8d422f96166dd6e9a
Ivan(Laptop) <ivan.teohjh@gmail.com>
2022-08-09 16:52:18 +0800

    base of ft_printf

---
+++ core/ft_printf_backup/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 16:41:33 by jteoh             #+#    #+#             */
+/*   Updated: 2022/08/09 16:41:33 by jteoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "./libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+#endif
