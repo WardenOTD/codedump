ff3fae14bc3464c755774c20113e197826263cee
jteoh <jteoh@student.42kl.edu.my>
2023-03-23 17:46:31 +0800

    yes

--- core/minitalk_backup/client.c
+++ core/minitalk_backup/client.c
@@ -6,7 +6,49 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:51:14 by jteoh             #+#    #+#             */
-/*   Updated: 2023/03/21 13:51:15 by jteoh            ###   ########.fr       */
+/*   Updated: 2023/03/23 17:38:51 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include "libft/libft.h"
+
+void	binary(int pid, char *str)
+{
+	int	num;
+	int	bit;
+	int	power;
+	int	count;
+
+	num = ft_atoi(str);
+	bit = 8;
+	while (bit > 0)
+	{
+		power = 2;
+		count = bit;
+		if (bit == 1)
+			power = 1;
+		while (count-- > 2)
+			power *= 2;
+		if (num / power == 0)
+			kill(pid, SIGUSR1);
+		else if (num / power == 1)
+			kill(pid, SIGUSR2);
+		num %= power;
+		bit--;
+		usleep(500);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	pid = ft_atoi(argv[1]);
+	if (argc == 3)
+	{
+		binary(pid, argv[2]);
+	}
+	return (0);
+}
--- core/minitalk_backup/server.c
+++ core/minitalk_backup/server.c
@@ -6,18 +6,33 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:51:17 by jteoh             #+#    #+#             */
-/*   Updated: 2023/03/21 14:08:20 by jteoh            ###   ########.fr       */
+/*   Updated: 2023/03/23 17:37:54 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include "ft_printf.h"
+#include <signal.h>
+#include "libft/libft.h"
+
+void	sig(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_putstr_fd("0\n", 1);
+	else if (sig == SIGUSR2)
+		ft_putstr_fd("1\n", 1);
+}
 
 int	main(void)
 {
 	int	pid;
-	
+
+	signal(SIGUSR1, sig);
+	signal(SIGUSR2, sig);
 	pid = getpid();
-	ft_printf("%d\n", pid);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+	while (1)
+	{
+	}
 	return (0);
 }
