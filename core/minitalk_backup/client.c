6ed51d4fbae300799ad88851bc343c566dca079a
jteoh <jteoh@student.42kl.edu.my>
2023-03-24 17:18:53 +0800

    considered done but a bit quirky

--- core/minitalk_backup/client.c
+++ core/minitalk_backup/client.c
@@ -6,49 +6,57 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:51:14 by jteoh             #+#    #+#             */
-/*   Updated: 2023/03/23 17:38:51 by jteoh            ###   ########.fr       */
+/*   Updated: 2023/03/24 16:55:40 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
+#include <stdio.h>
 #include "libft/libft.h"
 
 void	binary(int pid, char *str)
 {
 	int	num;
-	int	bit;
-	int	power;
 	int	count;
 
-	num = ft_atoi(str);
-	bit = 8;
-	while (bit > 0)
+	while (*str != 0)
 	{
-		power = 2;
-		count = bit;
-		if (bit == 1)
-			power = 1;
-		while (count-- > 2)
-			power *= 2;
-		if (num / power == 0)
-			kill(pid, SIGUSR1);
-		else if (num / power == 1)
-			kill(pid, SIGUSR2);
-		num %= power;
-		bit--;
-		usleep(500);
+		num = *str;
+		count = -1;
+		while (++count < 8)
+		{
+			if (num & 128)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			num <<= 1;
+			usleep(1750);
+		}
+		str++;
 	}
+}
+
+void	done(int sig)
+{
+	if (sig)
+		write(1, "\n\n=====done=====\n\n\n", (19 * sizeof(char)));
 }
 
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	eof;
 
+	signal(SIGUSR1, done);
+	eof = 8;
 	pid = ft_atoi(argv[1]);
 	if (argc == 3)
-	{
 		binary(pid, argv[2]);
+	while (eof-- > 0)
+	{
+		kill(pid, SIGUSR2);
+		usleep(500);
 	}
 	return (0);
 }
--- core/minitalk_backup/server.c
+++ core/minitalk_backup/server.c
@@ -6,28 +6,50 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:51:17 by jteoh             #+#    #+#             */
-/*   Updated: 2023/03/23 17:37:54 by jteoh            ###   ########.fr       */
+/*   Updated: 2023/03/24 17:14:26 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include "libft/libft.h"
+#include <stdio.h>
 
-void	sig(int sig)
+void	sig(int sig, siginfo_t *info, void *bruh)
 {
+	static int	bit = 0;
+	static int	count = 0;
+
+	(void)bruh;
 	if (sig == SIGUSR1)
-		ft_putstr_fd("0\n", 1);
-	else if (sig == SIGUSR2)
-		ft_putstr_fd("1\n", 1);
+		bit |= 0;
+	if (sig == SIGUSR2)
+		bit |= 1;
+	if (++count < 8)
+		bit <<= 1;
+	if ((bit & 255) == 255 && count == 8)
+	{
+		kill(info->si_pid, SIGUSR1);
+		count = 0;
+		bit = 0;
+	}
+	if (count == 8)
+	{
+		write(1, &bit, 1);
+		count = 0;
+		bit = 0;
+	}
 }
 
 int	main(void)
 {
-	int	pid;
+	int					pid;
+	struct sigaction	cringe;
 
-	signal(SIGUSR1, sig);
-	signal(SIGUSR2, sig);
+	cringe.sa_sigaction = sig;
+	cringe.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &cringe, NULL);
+	sigaction(SIGUSR2, &cringe, NULL);
 	pid = getpid();
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
